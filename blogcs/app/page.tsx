fdedce5644f09729fc59e633fd7702f17954a490
ahmedtamer <alqad.a.119@isg.edu.sa>
2024-03-30 22:40:48 -0500

    Seems like browser tool doesn;t show frontend changes, useffect not working, button click not working

--- blogcs/app/page.tsx
+++ blogcs/app/page.tsx
@@ -1,10 +1,10 @@
 "use client"
 import { useEffect } from "react";
+import React from "react";
 import connecttodb from "@/utils/connection";
 export default function Page() {
   const fetchBlog = async () => {
     try {
-      const connect=await connecttodb()
       const response = await fetch("http://localhost:3000/api/getblog",{
         method:"GET"
       });
@@ -14,14 +14,31 @@ export default function Page() {
       console.error(error);
     }
   };
+  const addpost=async()=>{
+    console.log("Frontend")
+    const insert=await fetch("http://localhost:3000/api/postblog",{
+      method:"POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify({
+        "postuser":"Ahmed",
+        "post":"Ahmed 2",
+        "likes":0
+      })
+    })
+  }
 
   useEffect(() => {
     fetchBlog();
   }, []);
-  fetchBlog()
+
+
   return (
     <div className="flex bg-slate-500">
       <h1>Hello CS Welcome</h1>
+      <button onClick={addpost}>Add Post</button>
+      <button onClick={()=>{window.location.reload()}}>TEST</button>
     </div>
   );
 }
