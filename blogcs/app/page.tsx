40dc0f1326aa1078145d746485ee1fc457326aef
ahmedtamer <alqad.a.119@isg.edu.sa>
2024-04-02 09:18:13 -0500

    fixed tailwind problem, added user post insert, reset mode. TODO: Authentication,Update,DeleteById,CSS

--- blogcs/app/page.tsx
+++ blogcs/app/page.tsx
@@ -2,8 +2,17 @@
 import { useEffect, useState } from "react";
 import React from "react";
 import connecttodb from "@/utils/connection";
+import "../app.css"
 export default function Page() {
   const [blog,setblog]=useState([]);
+  const [post,setpost]=useState("");
+  const [postuser,setpostuser]=useState("");
+  const reset=async()=>{
+    const deletes=await fetch("http://localhost:3000/api/deleteall",{
+      method:"DELETE"
+    })
+    console.log(deletes)
+  }
   const fetchBlog = async () => {
     try {
       const response = await fetch("http://localhost:3000/api/getblog",{
@@ -25,13 +34,14 @@ export default function Page() {
         'Content-Type': 'application/json'
       },
       body:JSON.stringify({
-        "post":"Ahmed",
-        "postuser":"Ahmed",
+        "post":post,
+        "postuser":postuser,
         "likes":"0"
       })
     })
     const insertf=await insert.json()
-    console.log(insertf)
+    setblog([...blog, { _id: insertf.id, post, postuser }]);
+    const refetch=await fetchBlog()
     } catch (error) {
       console.log(error)
     }
@@ -40,19 +50,36 @@ export default function Page() {
   useEffect(() => {
     const getposts=async()=>{
       const posts=await fetchBlog();
-      for(let i=0;i<10;i++){
-        console.log(posts[i]["post"])
-      }
+      setblog(posts)
     }
     getposts() 
   }, []);
   // fetchBlog()
 
   return (
-    <div className="flex bg-slate-500">
+    <div className="">
       <h1>Hello CS Welcome</h1>
-      <button onClick={addpost}>Add Post</button>
-      <button onClick={()=>{window.location.reload()}}>TEST</button>
+      <div className="">
+        <h1>Your Daily Blogs</h1>
+        <ul>
+          {blog.map(post=>
+            <li key={post["_id"]}>
+              {post["post"]}
+              <div>{post["postuser"]}</div>
+            </li>
+          )}
+        </ul>
+      </div>
+      <div>
+        <h1>Add A Blog</h1>
+        <label>What post would you like to share? </label>
+        <input onChange={(e)=>setpost(e.target.value)}></input>
+        <label>What name would you like to share? </label>
+        <input onChange={(e)=>setpostuser(e.target.value)}></input>
+      </div>
+      <button onClick={()=>addpost("fucking eh","isaac","0")}>Add Post</button>
+      <button onClick={()=>console.log(post)}>TEST</button>
+      <button onClick={()=>reset()}>Clear DB</button>
     </div>
   );
 }
---
+++ blogcs/pages/api/deleteall.ts
@@ -0,0 +1,17 @@
+import { blogmodel } from "@/schemas/blogschema";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler (req:NextApiRequest,res:NextApiResponse){
+    try {
+        const deleteall=await blogmodel.collection.deleteMany({})
+        if(deleteall){
+            res.json({deleteall})
+        }
+        else{
+            res.json({"error":"404"})
+        }
+    } catch (error) {
+        console.log(error)
+    }
+    
+}
