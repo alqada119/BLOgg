a09145f87a948413a34b3fdce8a4803092fe12eb
ahmedtamer <alqad.a.119@isg.edu.sa>
2024-04-15 20:17:11 -0500

    Looks semi decent

--- blogcs/app/[id]/page.tsx
+++ blogcs/app/[id]/page.tsx
@@ -51,24 +51,26 @@ export default function PostId(){
     },[])
     return (<>
     {/* TODO:CSS THIS AND ADD COMMENT */}
-    <div className="flex flex-col items-center justify-center h-screen">
-    <div className="flex flex-col items-center bg-orange-300 border-2 w-80">
-        <div className="flex "><div className="font-bold capitalize text-xl">{post?.post}</div>
-        <div className=" capitalize font-semibold text-l">{post?.postuser}</div>
-        <div className="font-bold">{post?.likes}</div></div>
+    <div className="flex flex-col items-center justify-center h-screen border-2 border-black">
+    <div className="flex flex-col items-center justify-center bg-slate-200 border-2 w-80 border-black h-1/3 w-1/3 mb-10 w-auto p-4">
+        <div className="flex items-center gap-10 bg-emerald-400 border-2 p-4 text-purple-400 border-black">
+            <div className="font-bold capitalize text-xl">{post?.post}</div>
+            <div className=" capitalize font-semibold text-l">{post?.postuser}</div>
+            <div className="font-bold">{post?.likes}</div>
+        </div>
    
-    <div>
-        {post?.comments.map(comment=>(
-            <div>
-                {comment}
+    <div className="flex gap-5 mt-5">
+        {post?.comments.map((comment,index)=>(
+            <div key={index} className="font-mono ">
+                {index}-{comment} 
             </div>
         ))}
     </div>
     </div>
     <div className="flex gap-3 items-center">
-        <label>Comment</label>
+        <label className=" font-bold font-mono">Comment</label>
         <input onChange={(e)=>setcomment(e.target.value)} className="border"></input>
-        <button onClick={()=>addcomment()} className="rounded-md shadow-md bg-black text-cyan-600 font-semibold p-4">Comment</button>
+        <button onClick={()=>addcomment()} className="rounded-md shadow-md bg-emerald-400 text-purple-400 font-semibold p-4">Comment</button>
         {/* <button onClick={()=>console.log(post?.comments)}className="rounded-md shadow-md bg-black text-cyan-600 font-semibold p-4" >Test COmments</button> */}
     </div>
     </div>
