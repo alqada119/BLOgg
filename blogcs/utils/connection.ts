911ea276cdf16fe2dc105562685aa437a4434af7
ahmedtamer <alqad.a.119@isg.edu.sa>
2024-06-07 22:42:16 -0500

    n

--- blogcs/utils/connection.ts
+++ blogcs/utils/connection.ts
@@ -1,19 +1,25 @@
 import mongoose from "mongoose";
-import * as dotenv from 'dotenv';
-export default async function connecttodb() {
-    dotenv.config()
-    console.log("Here is ",process.env.mongourl)
+import * as dotenv from "dotenv";
+
+dotenv.config();
+
+async function connecttodb(): Promise<typeof mongoose | void> {
+    console.log("Here is ", process.env.MONGODB_URL);
+
     try {
-        const mongourl=process.env.mongourl
-        const connect=await mongoose.connect(mongourl,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-        console.log("Succesful")
-        return connect
+        const mongoUrl = process.env.MONGODB_URL;
+        if (mongoUrl) {
+            const connection = await mongoose.connect(mongoUrl);
+            console.log("Successful connection to the database");
+            return connection;
+        } else {
+            console.error("MongoDB URL not provided in environment variables.");
+            process.exit(1);
+        }
     } catch (error) {
-        console.log(error)
-        process.exit(1)
+        console.error("Error connecting to the database: ", error);
+        process.exit(1);
     }
-    
 }
+
+export default connecttodb;
